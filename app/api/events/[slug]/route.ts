b9e3b3d998044c0b8d265b8d705a10a7cfb03388
unknown <csikosandras3@gmail.com>
2026-01-27 07:46:24 +0100

    eventDetails

---
+++ app/api/events/[slug]/route.ts
@@ -0,0 +1,40 @@
+import connectDB from "@/lib/actions/mongodb";
+import Event from '../../../../database/event.model';
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: Promise<{ slug: string }> }
+) {
+    try {
+        await connectDB();
+
+        const { slug } = await params;
+
+        if (!slug) {
+            return NextResponse.json(
+                { message: 'Slug not found' },
+                { status: 400 }
+            );
+        }
+
+        const event = await Event.findOne({ slug }).lean();
+
+        if (!event) {
+            return NextResponse.json(
+                { message: 'Event not found' },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json(
+            { message: 'Event fetched successfully', event },
+            { status: 200 }
+        );
+    } catch (e) {
+        return NextResponse.json(
+            { message: 'Event fetching failed', error: e instanceof Error ? e.message : 'Unknown error' },
+            { status: 500 }
+        );
+    }
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'res.cloudinary.com'
+    }]
+  },
   experimental: {
     turbopackFileSystemCacheForDev: true,
   }
